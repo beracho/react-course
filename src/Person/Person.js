26556bb677becba745512434259b6bdd9a89505f
Adrian Marcelo Berazain Mallea <marcelo.berazain@gmail.com>
2020-04-10 10:45:09 -0400

    Create custom component

---
+++ src/Person/Person.js
@@ -0,0 +1,21 @@
+// import React, { Component } from 'react'// rcc
+
+// export default class Person extends Component {
+//     render() {
+//         return (
+//             <div>
+                
+//             </div>
+//         )
+//     }
+// }
+
+import React from 'react' // rfce
+
+const person = () => {
+    return (
+        <p>I'm a person</p>
+    )
+}
+
+export default person
