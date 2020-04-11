04944f1c649a527b003d6160c6470c1fb05e81cf
Adrian Marcelo Berazain Mallea <marcelo.berazain@gmail.com>
2020-04-11 11:58:31 -0400

    Set hooks isntead of states

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+      persons: [
+        { name: 'Max', age: RandomAge() },
+        { name: 'Jorge', age: RandomAge() },
+        { name: 'Jie', age: RandomAge() },
+      ]
+  });
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Max', age: this.RandomAge() },
-      { name: 'Jorge', age: this.RandomAge() },
-      { name: 'Jie', age: this.RandomAge() },
-    ]
-  }
+  const [anotherState, setAnotherState] = useState('some other values');
+  console.log(personsState);
+  console.log(anotherState);
   
-  SwitchNameHandler = () => {
-    this.setState({persons: [
-      { name: 'Maximilian', age: this.RandomAge() },
-      { name: 'Jorge', age: this.RandomAge() },
-      { name: 'Jie', age: this.RandomAge() },
-    ]});
+  const SwitchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: RandomAge() },
+        { name: 'Jorge', age: RandomAge() },
+        { name: 'Jie', age: RandomAge() },
+      ]
+    });
   }
 
-  // RandomAge() {
-  //   return Math.floor(Math.random() * 30) + 15;
-  // }
-  RandomAge = () => {
-    return Math.floor(Math.random() * 30) + 15;
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>Can we do this?</p>
-        <button onClick={this.SwitchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>Can we do this?</p>
+      <button onClick={SwitchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
+const RandomAge = () => {
+  return Math.floor(Math.random() * 30) + 15;
+}
 
 export default App;
