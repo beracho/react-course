18360a4c7131aee0ff10309eebc37c09d34e37a3
Adrian Marcelo Berazain Mallea <marcelo.berazain@gmail.com>
2020-04-10 22:25:26 -0400

    class properties and methods

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>Can we do this?</p>
-      <Person name="Max" age ={RandomAge()}/>
-      <Person name="Manu" age ={RandomAge()}>Hobbies: Racing</Person>
-      <Person name="Jie" age ={RandomAge()}/>
-    </div>
-  );
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'))
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: this.RandomAge() },
+      { name: 'Jorge', age: this.RandomAge() },
+      { name: 'Jie', age: this.RandomAge() },
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>Can we do this?</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+  RandomAge() {
+    return Math.floor(Math.random() * 30) + 15;
+  }
 }
 
-const RandomAge = () => {
-  return Math.floor(Math.random() * 30) + 15;
-}
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -14,7 +14,10 @@ import React from 'react' // rfce
 
 const person = (props) => {
     return (
-        <p>I'm {props.name} and I am {props.age} years old</p>
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
     )
 }
 
