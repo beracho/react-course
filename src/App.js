1275a655d7df8304833e2ac851ffbdd9358b403e
Adrian Marcelo Berazain Mallea <marcelo.berazain@gmail.com>
2020-04-11 11:20:49 -0400

    sets onclick on button

--- src/App.js
+++ src/App.js
@@ -10,20 +10,33 @@ class App extends Component {
       { name: 'Jie', age: this.RandomAge() },
     ]
   }
+  
+  SwitchNameHandler = () => {
+    this.setState({persons: [
+      { name: 'Maximilian', age: this.RandomAge() },
+      { name: 'Jorge', age: this.RandomAge() },
+      { name: 'Jie', age: this.RandomAge() },
+    ]});
+  }
+
+  // RandomAge() {
+  //   return Math.floor(Math.random() * 30) + 15;
+  // }
+  RandomAge = () => {
+    return Math.floor(Math.random() * 30) + 15;
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Can we do this?</p>
-        <button>Switch Name</button>
+        <button onClick={this.SwitchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
-  }
-  RandomAge() {
-    return Math.floor(Math.random() * 30) + 15;
   }
 }
 
