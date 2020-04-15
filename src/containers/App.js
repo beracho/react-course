68411299d4c3a70b52653b8cdbf7c34f6e4b6179
Adrian Marcelo Berazain Mallea <marcelo.berazain@gmail.com>
2020-04-15 18:52:55 -0400

    creating the component structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import FirstTask from './FirstTask/FirstTask'
-import SecondTask from './SecondTask/SecondTask'
-import Ongoing from './Ongoing/Ongoing'
+import FirstTask from '../components/FirstTask/FirstTask'
+import SecondTask from '../components/SecondTask/SecondTask'
+import Ongoing from '../components/Ongoing/Ongoing'
 import './App.css';
 class App extends Component {
 
